70d1a17cab78c76d8c949ae51e21d2f28e5c2de3
root <root@localhost.localdomain>
2021-09-26 13:33:19 +0530

    Streams

---
+++ Streams/streams1.java
@@ -0,0 +1,25 @@
+//Streams in Java
+import java.util.ArrayList;
+import java.util.Scanner;
+class Student
+{
+	int rollno;
+	String name;
+	int marks;
+	Student(int rollno,String name,int marks)
+	{
+		this.rollno=rollno;
+		this.name=name;
+		this.marks=marks;
+	}
+}
+class A
+{
+	public static void main(String args[])
+	{
+		ArrayList<Student> al=new ArrayList<>();
+		ArrayList<Integer> filteredMarks=new ArrayList<>();
+		System.out.print("");
+		System.out.print();
+	}
+}
