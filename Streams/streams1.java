59f6108d98363768f73263f83ca8c718f1d98b2b
Bishnu Prasad Singh <bpsingh4040@gmail.com>
2021-10-14 17:08:59 +0530

    14-Oct-2021

--- Streams/streams1.java
+++ Streams/streams1.java
@@ -17,9 +17,22 @@ class A
 {
 	public static void main(String args[])
 	{
+		Scanner sc=new Scanner(System.in);
 		ArrayList<Student> al=new ArrayList<>();
 		ArrayList<Integer> filteredMarks=new ArrayList<>();
-		System.out.print("Enter ");
-		System.out.print();
+		System.out.print("Enter the number of students: ");
+		int n=sc.nextInt();
+		for(int i=0; i<n; i++)
+		{
+			System.out.print("Enter the Student's Roll No: ");
+			int rollno=sc.nextInt();
+			System.out.print("Enter the Student's Name: ");
+			String name=sc.nextLine();
+			//sc.next();
+			System.out.print("Enter the Student's Marks: ");
+			int marks=sc.nextInt();
+			al.add(new Student(rollno,name,marks));
+		}
+		System.out.println(al);
 	}
 }
